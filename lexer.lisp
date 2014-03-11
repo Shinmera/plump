6a80f9237863530f13aaccb96acc872faca2730a
Shinmera <shinmera@tymoon.eu>
2014-03-11 21:44:42 +0100

    Stub

---
+++ lexer.lisp
@@ -0,0 +1,9 @@
+#|
+ This file is a part of Plump
+ (c) 2014 TymoonNET/NexT http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.tymoonnext.plump)
+
+(defvar *root* NIL)
