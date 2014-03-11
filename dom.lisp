f98a327098df0a3e278c5298465e8a9d2957680d
Shinmera <shinmera@tymoon.eu>
2014-03-11 21:44:38 +0100

    Primitive dom

---
+++ dom.lisp
@@ -0,0 +1,198 @@
+#|
+ This file is a part of Plump
+ (c) 2014 TymoonNET/NexT http://tymoon.eu (shinmera@tymoon.eu)
+ Author: Nicolas Hafner <shinmera@tymoon.eu>
+|#
+
+(in-package #:org.tymoonnext.plump)
+
+(defclass node ()
+  ()
+  (:documentation ""))
+
+(defclass nesting-node (node)
+  ((%children :initarg :children :initform (make-child-array) :accessor children))
+  (:documentation ""))
+
+(defclass child-node (node)
+  ((%parent :initarg :parent :initform (error "Parent required.") :accessor parent))
+  (:documentation ""))
+
+(defclass root (nesting-node)
+  ()
+  (:documentation ""))
+
+(defclass text-node (child-node)
+  ((%text :initarg :text :initform "" :accessor text))
+  (:documentation ""))
+
+(defclass comment (child-node)
+  ((%text :initarg :text :initform "" :accessor text))
+  (:documentation ""))
+
+(defclass element (nesting-node child-node)
+  ((%tag-name :initarg :tag-name :initform (error "Tag name required.") :accessor tag-name)
+   (%attributes :initarg :attributes :initform (make-attribute-map) :accessor attributes))
+  (:documentation ""))
+
+(defun make-child-array ()
+  (make-array 0 :adjustable T :fill-pointer 0))
+
+(defun make-attribute-map ()
+  (make-hash-table :test 'equalp))
+
+(defun make-root (&optional (children (make-child-array)))
+  (make-instance 'root :children children))
+
+(defun make-element (parent tag &key (children (make-child-array)) (attributes (make-attribute-map)))
+  (make-instance 'element :tag tag :parent parent :children children :attributes attributes))
+
+(defun make-text-node (parent &optional (text ""))
+  (make-instance 'text-node :text text :parent parent))
+
+(defun make-comment (parent &optional (text ""))
+  (make-instance 'comment :text text :parent parent))
+
+(defun siblings (child)
+  (children (parent child)))
+
+(defun child-position (child)
+  (position child (siblings child)))
+
+(defun append-child (parent child)
+  (setf (parent child) parent)
+  (vector-push-extend child (children parent)))
+
+(defun prepend-child (parent child)
+  (setf (parent child) parent)
+  (vector-push-extend-front child (children parent)))
+
+(defun remove-child (child)
+  (setf (parent child) NIL)
+  (vector-pop-position
+   (siblings child)
+   (child-position child))
+  child)
+
+(defun replace-child (old-child new-child)
+  (setf (parent new-child) (parent old-child)
+        (elt (siblings old-child) (child-position old-child)) new-child
+        (parent old-child) NIL)
+  old-child)
+
+(defun insert-before (element new-child)
+  (vector-push-extend-position
+   new-child
+   (siblings element)
+   (child-position element))
+  new-child)
+
+(defun insert-after (element new-child)
+  (vector-push-extend-position
+   new-child
+   (siblings element)
+   (1+ (child-position element)))
+  new-child)
+
+(defun clone-children (node &key deep)
+  (loop with array = (make-array (length (children node)) :adjustable T :fill-pointer 0)
+        for child across (children node)
+        do (vector-push (if deep (clone-node child) child) array)
+        finally (return array)))
+
+(defun clone-attributes (node)
+  (alexandria:copy-hash-table (attributes node) :test 'equalp))
+
+(defgeneric clone-node (node &key deep)
+  (:method ((node comment) &key (deep T))
+    (make-instance (class-of node)
+                   :parent (parent node)
+                   :text (if deep (copy-seq (text node)) (text node))))
+  (:method ((node text-node) &key (deep T))
+    (make-instance (class-of node)
+                   :parent (parent node)
+                   :text (if deep (copy-seq (text node)) (text node))))
+  (:method ((node nesting-node) &key deep)
+    (make-instance (class-of node)
+                   :children (clone-children node :deep deep)))
+  (:method ((node element) &key deep)
+    (make-instance (class-of node)
+                   :tag-name (tag-name node)
+                   :children (clone-children node :deep deep)
+                   :attributes (clone-attributes node))))
+
+(defun first-child (element)
+  (elt (children element) 0))
+
+(defun last-child (element)
+  (elt (children element) (1- (fill-pointer (children element)))))
+
+(defun previous-sibling (child)
+  (elt (siblings child)
+       (1- (child-position child))))
+
+(defun next-sibling (child)
+  (elt (siblings child)
+       (1+ (child-position child))))
+
+(defun has-child-nodes (node)
+  (and (slot-boundp node '%children)
+       (< 0 (length (children node)))))
+
+(defun attribute (element attribute)
+  (gethash attribute (attributes element)))
+
+(defgeneric (setf attribute) (value element attribute)
+  (:method (value (element element) attribute)
+    (setf (gethash attribute element) value)))
+
+(defun get-attribute (element attribute)
+  (attribute element attribute))
+
+(defun set-attribute (element attribute value)
+  (setf (attribute element attribute) value))
+
+(defun remove-attribute (element attribute)
+  (remhash attribute element))
+
+(defun has-attribute (element attribute)
+  (not (null (gethash attribute element))))
+
+(defun get-elements-by-tag-name (node tag)
+  (let ((finds ()))
+    (labels ((scan-children (node)
+               (loop for child across (children node)
+                     do (when (element-p child)
+                          (when (string-equal tag (tag-name child))
+                            (push child finds))
+                          (scan-children child)))))
+      (scan-children node))
+    finds))
+
+(defun get-element-by-id (node id)
+  (let ((finds ()))
+    (labels ((scan-children (node)
+               (loop for child across (children node)
+                     do (when (element-p child)
+                          (let ((cid (attribute child "id")))
+                            (when (string-equal id cid)
+                              (push child finds)))
+                          (scan-children child)))))
+      (scan-children node))
+    finds))
+
+
+(defun node-p (object)
+  (typep object 'node))
+
+(defun element-p (object)
+  (typep object 'element))
+
+(defun text-node-p (object)
+  (typep object 'text-node))
+
+(defun comment-p (object)
+  (typep object 'comment))
+
+(defun root-p (object)
+  (typep object 'root))
