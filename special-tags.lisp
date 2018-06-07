d45b039bf59bcc7c364a44866d8b5b21e06b3fb1
Shinmera <shinmera@tymoon.eu>
2018-06-07 20:08:05 +0200

    Fucking html

--- special-tags.lisp
+++ special-tags.lisp
@@ -143,7 +143,7 @@
 
 (macrolet ((define-all (&rest tags)
              `(progn ,@(loop for tag in tags collect `(define-grouping-element ,tag *tag-dispatchers* *html-tags*)))))
-  (define-all blockquote dd dir div span dl dt figcaption figure hr li main ol p pre ul))
+  (define-all blockquote dd dir div span dl dt figcaption figure hr li main ol p pre ul code pre i))
 
 (defun read-fulltext-element-content (name)
   (with-output-to-string (out)
