3c8d1156961dae3afe7ab8b8972b0b514a490377
charJe <45857837+charJe@users.noreply.github.com>
2021-01-23 02:06:46 -0600

    Use Generic Wild Card Tag Dispatcher (#32)
    * Add wild card dispatcher for *html-tags* and default *tag-dispatchers*
    
    * Make wild card dispatcher be selected only by being the last one

--- tag-dispatcher.lisp
+++ tag-dispatcher.lisp
@@ -21,13 +21,17 @@
   (find name list :key #'tag-dispatcher-name))
 
 (define-setf-expander tag-dispatcher (name &optional (list '*all-tag-dispatchers*))
-  (let ((nameg (gensym "NAME"))
-        (disp (gensym "DISP")))
+  (let* ((nameg (gensym "NAME"))
+         (disp (gensym "DISP"))
+         (removed (gensym)))
     (values (list nameg)
             (list name)
             (list disp)
-            `(progn
-               (setf ,list (list* ,disp (remove ,nameg ,list :key #'tag-dispatcher-name)))
+            `(let ((,removed (remove ,nameg ,list :key #'tag-dispatcher-name)))
+               (setf ,list
+                     (if (eq '* ,nameg)
+                         (append ,removed (list ,disp))
+                         (list* ,disp ,removed)))
                ,disp)
             disp)))
 
